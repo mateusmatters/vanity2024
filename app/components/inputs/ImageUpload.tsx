9df50a058d7df8bdf1f981fdccfe8eb0ec9aa9a8
Mateus Kelly <mkelly20@umd.edu>
2024-04-02 17:39:39 -0400

    first commit

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 
 import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useCallback } from "react";
-import { TbPhotoPlus } from 'react-icons/tb'
+import { TbPhotoPlus } from "react-icons/tb";
 
 declare global {
-  var cloudinary: any
+  var cloudinary: any;
 }
 
 const uploadPreset = "pgc9ehd5";
@@ -16,20 +16,21 @@ interface ImageUploadProps {
   value: string;
 }
 
-const ImageUpload: React.FC<ImageUploadProps> = ({
-  onChange,
-  value
-}) => {
-  const handleUpload = useCallback((result: any) => {
-    onChange(result.info.secure_url);
-  }, [onChange]);
+const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
+  const handleUpload = useCallback(
+    (result: any) => {
+      onChange(result.info.secure_url);
+    },
+    [onChange]
+  );
 
   return (
-    <CldUploadWidget 
-      onUpload={handleUpload} 
-      uploadPreset={uploadPreset}
+    <CldUploadWidget
+      onUpload={handleUpload}
+      //Got this uploadPreset from my cloudinary account
+      uploadPreset="lfnqnhyp"
       options={{
-        maxFiles: 1
+        maxFiles: 1,
       }}
     >
       {({ open }) => {
@@ -53,28 +54,26 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
               text-neutral-600
             "
           >
-            <TbPhotoPlus
-              size={50}
-            />
-            <div className="font-semibold text-lg">
-              Click to upload
-            </div>
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload</div>
             {value && (
-              <div className="
-              absolute inset-0 w-full h-full">
+              <div
+                className="
+              absolute inset-0 w-full h-full"
+              >
                 <Image
-                  fill 
-                  style={{ objectFit: 'cover' }} 
-                  src={value} 
-                  alt="House" 
+                  fill
+                  style={{ objectFit: "cover" }}
+                  src={value}
+                  alt="House"
                 />
               </div>
             )}
           </div>
-        ) 
-    }}
+        );
+      }}
     </CldUploadWidget>
   );
-}
+};
 
 export default ImageUpload;
